421b05f480b3545121d0abe5a3c04a062f4baf77
Ted <ted_hodges@ymail.com>
2018-04-28 13:15:30 -0500

    set up directory and first attempt

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Animal Style Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baco-Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', true);
+
